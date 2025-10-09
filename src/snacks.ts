68a959cd1b8164ac9b1f0e9458da394ba2067364
MajesticWater <147297606+MajesticWater@users.noreply.github.com>
2025-10-09 12:44:52 -0700

    added all snacks

--- src/snacks.ts
+++ src/snacks.ts
@@ -1,6 +1,10 @@
-let snacks: string[] = ['watermelon', 'tortilla chips', 'salsa']
+import { animation } from "./animation";
+
+let snacks: string[] = ['watermelon', 'tortilla chips', 'salsa', 'oranges', 'string cheese', 
+    'brie cheese', 'crackers'];
 
 export function print_snacks(): void {
+    animation("snacks");
     console.log('Snacks: ');
     for (let snack of snacks) {
         console.log(snack);
