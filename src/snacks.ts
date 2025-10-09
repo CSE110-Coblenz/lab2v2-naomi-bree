0a4776daf382fcb4601d6a206dfb47356332d391
naomimetzler <nmetzler@ucsd.edu>
2025-10-09 12:32:47 -0700

    Printed animation before snacks

--- src/snacks.ts
+++ src/snacks.ts
@@ -1,6 +1,9 @@
+import { animation } from "./animation";
+
 let snacks: string[] = ['watermelon', 'tortilla chips', 'salsa']
 
 export function print_snacks(): void {
+    animation("snacks");
     console.log('Snacks: ');
     for (let snack of snacks) {
         console.log(snack);
