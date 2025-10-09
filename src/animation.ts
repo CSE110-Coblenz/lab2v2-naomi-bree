f56e69e3beb07185c2baf8cf80ff3d5b9776a6f0
MajesticWater <147297606+MajesticWater@users.noreply.github.com>
2025-10-09 12:09:33 -0700

    created animation file

---
+++ src/animation.ts
@@ -0,0 +1,3 @@
+export function animation(category : string) {
+    console.log(`\x1B[1m\x1B[3mYayyy ${category} time!!! Woooo~~ :D\x1B[22m\x1B[23m`);
+}
--- src/decorations.ts
+++ src/decorations.ts
@@ -1,7 +1,10 @@
+import { animation } from "./animation";
+
 let decorationList: string[] = ['streamers', 'balloons', 'confetti', 
     'tablecloths', 'flowers', 'flower vases'];
 
 export function printDecorations() {
+    animation("decoration");
     console.log("Decorations:");
     for (let decoration of decorationList) {
         console.log(decoration);
