6eaffbf1682e4416ed4b03dea8ec5d98bc98ec37
naomimetzler <nmetzler@ucsd.edu>
2025-10-15 12:10:15 -0700

    got rid of a snack

--- src/snacks.ts
+++ src/snacks.ts
@@ -1,6 +1,6 @@
 import { animation } from "./animation";
 
-export let snacks: string[] = ['watermelon', 'tortilla chips', 'salsa']
+export let snacks: string[] = ['watermelon', 'chips']
 
 export function print_snacks(): void {
     animation("snacks");
