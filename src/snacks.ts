8fff57551403f7e980ed6b244a56278749c3c2b4
naomimetzler <nmetzler@ucsd.edu>
2025-10-15 12:05:09 -0700

    made ci.yml file and test files

--- src/snacks.ts
+++ src/snacks.ts
@@ -1,6 +1,6 @@
 import { animation } from "./animation";
 
-let snacks: string[] = ['watermelon', 'tortilla chips', 'salsa']
+export let snacks: string[] = ['watermelon', 'tortilla chips', 'salsa']
 
 export function print_snacks(): void {
     animation("snacks");
